c81f825e2e138e4ce57fb34a31bf57da271ec6e7
amandatrim <amanda162@usf.edu>
2024-09-20 16:10:23 +0100

    initialize employees with shift schedules

---
+++ employeeShiftManagement.js
@@ -0,0 +1,13 @@
+/*
+  Task 1
+  The first task of this assignment is to create an employee array, that contains information
+  about all the employees in the system 
+*/
+
+let employees = [
+    { name: 'Fuhad', shifts: [{ day: 'Monday', hours: 4 }, { day: 'Thursdays', hours: 8 }] },
+    { name: 'Sarah', shifts: [{ day: 'Wednesdays', hours: 5 }, { day: 'Fridays', hours: 12 }] },
+    { name: 'Daniel', shifts: [{ day: 'Monday', hours: 10 }, { day: 'Tuesdays', hours: 5 }] },
+    { name: 'James', shifts: [{ day: 'Tuesdays', hours: 9 }, { day: 'Thursdays', hours: 7 }, { day: 'Friday', hours: 8 }] }
+    { name: 'Elis', shifts: [{ day: 'Monday', hours: 7 }, { day: 'Tuesdays', hours: 5 }, { day: 'Thursdays', hours: 8 }] },
+]
